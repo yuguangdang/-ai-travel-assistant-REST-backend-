f0641706efca26c210f5f052a5aa71947577d54f
yuguang.dang <yuguang.dang@travelctm.com>
2024-05-31 18:38:09 +1000

    support both Teams and WhatsApp

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 from .message_routes import main as main_blueprint
+from .assistant_routes import assistant as assistant_blueprint
 
 def register_blueprints(app):
     app.register_blueprint(main_blueprint)
+    app.register_blueprint(assistant_blueprint)
     # Register other blueprints as needed
